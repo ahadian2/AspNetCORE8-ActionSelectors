06d0909870613858d1da990ad252b12957c53b5b
Mohammadreza <ahadian2@gmail.com>
2024-04-01 22:34:05 +0330

    Add project files.

---
+++ ActionSelectors/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ActionSelectors.Controllers
+{
+    public class BlogController : Controller
+    {
+        //[NonAction]
+        [HttpPost]
+        public string Index1()
+        {
+            return "index1";
+        }
+        [HttpGet]
+        public string Index2()
+        {
+            return "index2";
+        }
+        [ActionName("Login")]
+        [AcceptVerbs("Post","Get","Put")]
+        public IActionResult Index3()
+        {
+            return View("Index3");
+        }
+    }
+}
